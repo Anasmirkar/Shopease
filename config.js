1531e51af0c708bf573bef0e0eb5373176667a79
Anasmirkar <certifytube.dev@gmail.com>
2025-10-31 21:06:37 +0530

    Update Google OAuth with new Android client ID

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 export const GOOGLE_AUTH_CONFIG = {
   // Use Android client ID for standalone builds
-  androidClientId: '142289263449-7fd303hf91qe07h59h3g031q0sjhq11u.apps.googleusercontent.com',
+  androidClientId: '142289263449-v0hig021veh450nvrigav2hp7u8ard8e.apps.googleusercontent.com',
   // Keep web client for any web usage
   webClientId: '142289263449-1c3hvknhgvs4m2js9u336bhnsfu0gcvh.apps.googleusercontent.com',
   scopes: ['openid', 'profile', 'email'],
